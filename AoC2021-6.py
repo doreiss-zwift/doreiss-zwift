7fd4d9ea7420d41e4b3bd2228abb96e000a32f2c
Dominic Reiss <91640681+doreiss-zwift@users.noreply.github.com>
2021-12-06 09:42:02 -0900

    Create AoC2021-6.py

---
+++ AoC2021-6.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+
+fish_map = defaultdict(lambda: 0)
+
+f = open("input.txt")
+init_fish_array = [int(s) for s in f.readlines()[0].split(',')]
+f.close()
+
+min_fish_value = 999
+for fish in init_fish_array:
+    fish_map[fish] += 1
+    if fish < min_fish_value:
+        min_fish_value = fish
+
+elapsed_days = 0
+total_days = 256
+result = len(init_fish_array)
+while True:
+    tmp = defaultdict(lambda: 0)
+    new_min_fish_value = 999
+    elapsed_days += (min_fish_value + 1)
+    if(elapsed_days > total_days):
+        print(result)
+        break
+    for key in sorted(fish_map) :
+        if key == min_fish_value :
+            tmp[8] += fish_map[min_fish_value]
+            tmp[6] += fish_map[min_fish_value]
+            result += fish_map[min_fish_value]
+            if 6 < new_min_fish_value :
+                new_min_fish_value = 6
+        else:
+            tmp[key - min_fish_value - 1] += fish_map[key]
+            if key - min_fish_value - 1 < new_min_fish_value :
+                new_min_fish_value = key - min_fish_value - 1
+    fish_map = tmp
+    min_fish_value = new_min_fish_value
